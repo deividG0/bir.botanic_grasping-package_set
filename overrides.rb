5e6fe5ce48c07a99ca87123be0f8f13c037edc47
Deivid Gomes <davidsilva29@hotmail.com>
2024-12-06 08:54:53 -0300

    fix config files

--- overrides.rb
+++ overrides.rb
@@ -1,3 +1,20 @@
 # Write in this file customization code that will get executed after all the
 # soures have beenloaded.
 
+package_list = [
+    "numpy",
+    "opencv-python",
+    "pyrealsense2",
+    "transformations",
+    "urdf-parser-py",
+    "trimesh",
+    "scikit-image"
+]
+
+Autoproj.manifest.each_autobuild_package do |pkg|
+    if pkg.kind_of?(Autobuild::Colcon)
+        package_list.each do |package|
+            pkg.depends_on package
+        end
+    end
+end
